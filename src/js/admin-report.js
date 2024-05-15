839b8d25cc88b431205fd19dd1b7e274bee64e6d
IsaacCastelo <isaacpxl8@gmail.com>
2024-05-15 04:40:28 -0700

    base functions

--- src/js/admin-report.js
+++ src/js/admin-report.js
@@ -1,3 +1,11 @@
 import { ServicioTarea } from "../../service/tareaService.js";
 import { CookieService } from "../../service/cookieService.js";
 import { Tarea } from "../DTO/tareaDTO.js"
+
+function getListadoDocente(){
+
+}
+
+function updateInformacion(){
+
+}
--- src/js/admin-verify.js
+++ src/js/admin-verify.js
@@ -1,3 +1,12 @@
 import { ServicioTarea } from "../../service/tareaService.js";
 import { CookieService } from "../../service/cookieService.js";
 import { Tarea } from "../DTO/tareaDTO.js"
+
+
+function getListadoTareas(){
+
+}
+
+function updateEstado(){
+
+}
